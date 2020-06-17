23ac9802e5e5575eb2cc0dedbdda9d2681b5b70d
David Lacarta <davidlacarta@gmail.com>
2020-06-17 02:11:18 +0200

    Change chat styles

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,7 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: "Calibri", "Roboto", sans-serif;
         }
 
         * {
