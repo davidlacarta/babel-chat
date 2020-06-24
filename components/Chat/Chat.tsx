b2850ca12b5935a46b2a486e59ef053acf36f92c
David Lacarta <davidlacarta@gmail.com>
2020-06-24 22:17:49 +0200

    Add grommet and remove old styles

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -4,10 +4,18 @@ import useMessages from "components/Chat/useMessages";
 import useLangs from "./useLangs";
 import scrollTop from "./helpers/scrollTop";
 import clearInput from "./helpers/clearInput";
-import Header from "./Header";
-import Messages from "./Messages";
-import Footer from "./Footer";
-import { TypingType } from "server/shared/types";
+import { TypingType, MessageType } from "server/shared/types";
+import { Send } from "grommet-icons";
+import {
+  Header,
+  Main,
+  Heading,
+  Grid,
+  Box,
+  Button,
+  TextInput,
+  FormField,
+} from "grommet";
 
 export type Props = {
   room?: string;
@@ -76,46 +84,109 @@ export default function Chat({ room }: Props) {
     sendMessage(text);
   }
 
+  const writtersWithoutMe = writters.filter((writter) => writter !== username);
+
   return (
-    <>
-      <div className="chat_window">
-        <Header
-          username={username}
-          room={room}
-          lang={lang}
-          toogleLang={toogleLang}
-        />
-        <Messages
-          username={username}
-          lang={lang}
-          messagesRef={messagesRef}
-          messages={messages}
-          writters={writters}
-        />
-        <Footer
-          username={username}
-          lang={lang}
-          messageRef={messageRef}
-          handleKeyDown={handleKeyDown}
-          handleClickAvatar={handleClickAvatar}
-          handleBlur={handleBlur}
-        />
-      </div>
-      <style jsx>{`
-        .chat_window {
-          position: absolute;
-          width: 100%;
-          max-width: 800px;
-          height: 100%;
-          border-radius: 10px;
-          background-color: #fff;
-          left: 50%;
-          top: 50%;
-          transform: translateX(-50%) translateY(-50%);
-          background-color: #f8f8f8;
-          overflow: hidden;
-        }
-      `}</style>
-    </>
+    <Grid fill rows={["xsmall", "auto"]}>
+      <Header>
+        <Box
+          direction="row"
+          fill="horizontal"
+          align="center"
+          alignContent="center"
+        >
+          <Heading>{(room && `🔒 ${room}`) || `Babel`}</Heading>
+          <span>{username?.slice(0, 3)}</span>
+          <button onClick={toogleLang}>{lang.flag}</button>
+        </Box>
+      </Header>
+      <Main>
+        <Grid fill rows={["auto", "xsmall"]}>
+          <Box background="light-3" overflow="auto" round margin="medium">
+            <ul ref={messagesRef}>
+              {messages.map(
+                ({ content, author, translation, type, createdAt }, index) => {
+                  switch (type) {
+                    case MessageType.USER_HAS_JOINED:
+                      return (
+                        <li
+                          key={index}
+                          dangerouslySetInnerHTML={{
+                            __html: `${lang.joined(content!)} <small>${format(
+                              createdAt,
+                              lang.locale
+                            )}</small>`,
+                          }}
+                        ></li>
+                      );
+                    case MessageType.USER_HAS_DISCONNECTED:
+                      return (
+                        <li
+                          key={index}
+                          dangerouslySetInnerHTML={{
+                            __html: `${lang.disconnected(
+                              content!
+                            )} <small>${format(
+                              createdAt,
+                              lang.locale
+                            )}</small>`,
+                          }}
+                        ></li>
+                      );
+                    default:
+                      return (
+                        <li key={index}>
+                          <span>{author?.slice(0, 3)}</span>
+                          <span>{content}</span>
+                          <small>{translation && translation[lang.code]}</small>
+                          <span>{format(createdAt, lang.locale)}</span>
+                        </li>
+                      );
+                  }
+                }
+              )}
+              {writtersWithoutMe.length > 0 && (
+                <li>{lang.typing(writtersWithoutMe)}</li>
+              )}
+            </ul>
+          </Box>
+          <Box
+            direction="row"
+            margin={{ horizontal: "medium", vertical: "small" }}
+            pad="medium"
+            align="center"
+            gap="medium"
+            as="form"
+            width={{ max: "800px" }}
+            onSubmit={(event) => event.preventDefault()}
+          >
+            <FormField fill="horizontal" margin="0">
+              <TextInput
+                placeholder={username ? lang.message : lang.username}
+                ref={messageRef}
+                onKeyDown={handleKeyDown}
+                onBlur={handleBlur}
+              />
+            </FormField>
+            <Box align="center">
+              <Button
+                type="submit"
+                primary
+                icon={<Send />}
+                onClick={handleClickAvatar}
+              />
+            </Box>
+          </Box>
+        </Grid>
+      </Main>
+    </Grid>
   );
+}
+
+function format(dateTime: Date, locale: string) {
+  const [hours, minutes] = new Date(dateTime)
+    .toLocaleTimeString(locale, { hour12: false })
+    .split(":");
+
+  return `${hours}:${minutes}`;
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,20 @@
 import App from "next/app";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
-import { defaultTheme } from "styleguide/theme";
 import Head from "next/head";
+import { Grommet, grommet as grommetTheme } from "grommet";
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <>
-        <GlobalStyle />
         <Head>
           <title>Babel</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <ThemeProvider theme={defaultTheme}>
+        <Grommet theme={grommetTheme} full>
           <Component {...pageProps} />
-        </ThemeProvider>
+        </Grommet>
       </>
     );
   }
 }
-
-const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: "Calibri", "Roboto", sans-serif;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-`;
