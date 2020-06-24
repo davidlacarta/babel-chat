9659df2ca13d7a36fe5ac09093ba7eb04cf6e4fb
David Lacarta <davidlacarta@gmail.com>
2020-06-24 22:35:15 +0200

    Remove jsx styles

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -4,10 +4,7 @@ import useMessages from "components/Chat/useMessages";
 import useLangs from "./useLangs";
 import scrollTop from "./helpers/scrollTop";
 import clearInput from "./helpers/clearInput";
-import Header from "./Header";
-import Messages from "./Messages";
-import Footer from "./Footer";
-import { TypingType } from "server/shared/types";
+import { TypingType, MessageType } from "server/shared/types";
 
 export type Props = {
   room?: string;
@@ -76,46 +73,71 @@ export default function Chat({ room }: Props) {
     sendMessage(text);
   }
 
+  const writtersWithoutMe = writters.filter((writter) => writter !== username);
+
   return (
-    <>
-      <div className="chat_window">
-        <Header
-          username={username}
-          room={room}
-          lang={lang}
-          toogleLang={toogleLang}
-        />
-        <Messages
-          username={username}
-          lang={lang}
-          messagesRef={messagesRef}
-          messages={messages}
-          writters={writters}
-        />
-        <Footer
-          username={username}
-          lang={lang}
-          messageRef={messageRef}
-          handleKeyDown={handleKeyDown}
-          handleClickAvatar={handleClickAvatar}
-          handleBlur={handleBlur}
-        />
-      </div>
-      <style jsx>{`
-        .chat_window {
-          position: absolute;
-          width: 100%;
-          max-width: 800px;
-          height: 100%;
-          border-radius: 10px;
-          background-color: #fff;
-          left: 50%;
-          top: 50%;
-          transform: translateX(-50%) translateY(-50%);
-          background-color: #f8f8f8;
-          overflow: hidden;
-        }
-      `}</style>
-    </>
+    <div>
+      <h1>{(room && `🔒 ${room}`) || `Babel`}</h1>
+      <button onClick={toogleLang}>{lang.flag}</button>
+      <ul ref={messagesRef}>
+        {messages.map(
+          ({ content, author, translation, type, createdAt }, index) => {
+            switch (type) {
+              case MessageType.USER_HAS_JOINED:
+                return (
+                  <li
+                    key={index}
+                    dangerouslySetInnerHTML={{
+                      __html: `${lang.joined(content!)} <small>${format(
+                        createdAt,
+                        lang.locale
+                      )}</small>`,
+                    }}
+                  ></li>
+                );
+              case MessageType.USER_HAS_DISCONNECTED:
+                return (
+                  <li
+                    key={index}
+                    dangerouslySetInnerHTML={{
+                      __html: `${lang.disconnected(content!)} <small>${format(
+                        createdAt,
+                        lang.locale
+                      )}</small>`,
+                    }}
+                  ></li>
+                );
+              default:
+                return (
+                  <li key={index}>
+                    <div>{author?.slice(0, 3)}</div>
+                    <div>{content}</div>
+                    <div>{translation && translation[lang.code]}</div>
+                    <span>{format(createdAt, lang.locale)}</span>
+                  </li>
+                );
+            }
+          }
+        )}
+        {writtersWithoutMe.length > 0 && (
+          <li>{lang.typing(writtersWithoutMe)}</li>
+        )}
+      </ul>
+      <input
+        placeholder={username ? lang.message : lang.username}
+        ref={messageRef}
+        onKeyDown={handleKeyDown}
+        onBlur={handleBlur}
+      />
+      <button onClick={handleClickAvatar}>{lang.send}</button>
+    </div>
   );
+}
+
+function format(dateTime: Date, locale: string) {
+  const [hours, minutes] = new Date(dateTime)
+    .toLocaleTimeString(locale, { hour12: false })
+    .split(":");
+
+  return `${hours}:${minutes}`;
 }
