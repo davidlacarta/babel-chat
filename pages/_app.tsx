04e1cd3ec7af852da37568b310a5674bb870487f
David Lacarta <davidlacarta@gmail.com>
2020-06-28 13:12:40 +0200

    Fix mobile height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,32 @@ import Head from "next/head";
 import { Grommet, grommet as grommetTheme } from "grommet";
 import { StateProvider, reducer } from "../state";
 import { langs } from "i18n/langs";
+import { createGlobalStyle } from "styled-components";
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <StateProvider initialState={{ lang: langs.spain }} reducer={reducer}>
-        <Head>
-          <title>Babel</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Grommet theme={grommetTheme} full>
-          <Component {...pageProps} />
-        </Grommet>
-      </StateProvider>
+      <>
+        <GlobalStyle />
+        <StateProvider initialState={{ lang: langs.spain }} reducer={reducer}>
+          <Head>
+            <title>Babel</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Grommet theme={grommetTheme} full style={{ height: "100%" }}>
+            <Component {...pageProps} />
+          </Grommet>
+        </StateProvider>
+      </>
     );
   }
 }
+
+const GlobalStyle = createGlobalStyle`
+  html, 
+  body, 
+  body > div {
+    height: 100%;
+  }
+`;
